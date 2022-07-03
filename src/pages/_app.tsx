0b7383d95d5f4eabc64af20fc796266e5e8218ec
mayone-du <cocomayo1201@icloud.com>
2022-07-03 09:10:25 +0900

    🔥 自動生成された初期コードやファイルを削除

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "src/styles/tailwind.css";
+
+import type { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
